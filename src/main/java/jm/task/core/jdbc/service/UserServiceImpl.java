03e9e1199b059ebe961d3aae78327095b847e6de
Vasiliy <exs777@mail.ru>
2025-12-01 19:35:22 +0700

    hibernate

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,13 +1,15 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDao dao = new UserDaoJDBCImpl();
+    //UserDao dao = new UserDaoJDBCImpl();
+    UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,11 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,5 +18,23 @@ public class Util {
 
     public static Connection getConnect() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        try {
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                    .applySetting("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                    .applySetting("hibernate.connection.url", DB_URL)
+                    .applySetting("hibernate.connection.username", "root")
+                    .applySetting("hibernate.connection.password", "rootuser")
+                    .applySetting("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .applySetting("hibernate.show_sql", "true").build();
+            Metadata metadata = new MetadataSources(registry)
+                    .addAnnotatedClass(jm.task.core.jdbc.model.User.class)
+                    .buildMetadata();
+            return metadata.buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка создания SessionFactory", e);
+        }
     }
 }
